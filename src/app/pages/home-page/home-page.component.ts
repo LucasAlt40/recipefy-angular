e8b116e37c434ccbcf275d77230ab79bc91ee6a0
LucasAlt40 <lucas.alt40@gmail.com>
2024-01-21 01:02:04 -0300

    refactoring

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { SharedModule } from '../../shared/shared.module';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { CommonButtonComponent } from '../../shared/components/common-button/common-button.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [SharedModule],
+  imports: [CommonButtonComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
